dcbe15ec8f1d9a381982fb241ba88c804be2df6f
Trannick12222 <nicktran12345@gmail.com>
2025-06-28 13:51:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ def _e(keys, default=None):
     return default
 
 DB_CONFIG = {
-    "host":      _e(["MYSQL_HOST","MYSQLHOST"], "localhost"),
-    "port":      int(_e(["MYSQL_PORT","MYSQLPORT"], 3306)),
-    "user":      _e(["MYSQL_USER","MYSQLUSER"], "root"),
-    "password":  _e(["MYSQL_PASSWORD","MYSQLPASSWORD","MYSQL_ROOT_PASSWORD"], ""),
-    "db":        _e(["MYSQL_DATABASE"], "quanlytruonghoc_app"),
+    "host":      _e(["MYSQL_HOST", "MYSQLHOST"], "shuttle.proxy.rlwy.net"),
+    "port":      int(_e(["MYSQL_PORT", "MYSQLPORT"], 36286)),
+    "user":      _e(["MYSQL_USER", "MYSQLUSER"], "root"),
+    "password":  _e(["MYSQL_PASSWORD", "MYSQLPASSWORD", "MYSQL_ROOT_PASSWORD"], "kZwsdDWOrUWwXDamELJNjAAoUnzCtROz"),
+    "db":        _e(["MYSQL_DATABASE"], "railway"),
     "cursorclass": pymysql.cursors.DictCursor
 }
