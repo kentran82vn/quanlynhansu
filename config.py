195f4f6b2b10c8bb5ce7388872bfec025bb04639
Trannick12222 <nicktran12345@gmail.com>
2025-06-28 11:54:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os, pymysql
 
 DB_CONFIG = {
-    "host":     os.environ.get("MYSQL_HOST",    "localhost"),
+    "host":     os.environ.get("MYSQL_HOST",    "mysql.railway.internal"),
     "port":     int(os.environ.get("MYSQL_PORT", 3306)),
     "user":     os.environ.get("MYSQL_USER",    "root"),
     "password": os.environ.get("MYSQL_PASSWORD","steven2906"),
