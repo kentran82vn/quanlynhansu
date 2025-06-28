a649b4c942367a749e8601fdd61ba8f530750507
Trannick12222 <nicktran12345@gmail.com>
2025-06-28 11:25:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os, pymysql
 
 DB_CONFIG = {
     "host":     os.environ.get("MYSQL_HOST",    "localhost"),
-    "port":     int(os.environ.get("MYSQL_PORT", 33060)),
+    "port":     int(os.environ.get("MYSQL_PORT", 3306)),
     "user":     os.environ.get("MYSQL_USER",    "root"),
     "password": os.environ.get("MYSQL_PASSWORD","steven2906"),
     "db":       os.environ.get("MYSQL_DATABASE","quanlytruonghoc_app"),
