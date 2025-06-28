5b4046b0f7df323ac2a3c5fd49b77306c5df2f98
Trannick12222 <nicktran12345@gmail.com>
2025-06-28 11:03:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-import pymysql
+import os, pymysql
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "steven2906",  # đúng mật khẩu MySQL
-    "db": "quanlytruonghoc_app",  # ← sửa 'database' → 'db'
+    "host":     os.environ.get("MYSQL_HOST",    "localhost"),
+    "port":     int(os.environ.get("MYSQL_PORT", 3306)),
+    "user":     os.environ.get("MYSQL_USER",    "root"),
+    "password": os.environ.get("MYSQL_PASSWORD","steven2906"),
+    "db":       os.environ.get("MYSQL_DATABASE","quanlytruonghoc_app"),
     "cursorclass": pymysql.cursors.DictCursor
 }
