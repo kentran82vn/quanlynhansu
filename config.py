f3186a374403e6179fd1c6d85997c73b9230aa2a
anhquannewbie <nicktran12345@gmail.com>
2025-07-15 01:53:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
-import pymysql
+import os, pymysql
+
+def _e(keys, default=None):
+    for k in keys:
+        v = os.environ.get(k)
+        if v: return v
+    return default
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "steven2906",  # đúng mật khẩu MySQL
-    "db": "quanlytruonghoc_app",  # ← sửa 'database' → 'db'
+    "host":      _e(["MYSQL_HOST", "MYSQLHOST"], "shuttle.proxy.rlwy.net"),
+    "port":      int(_e(["MYSQL_PORT", "MYSQLPORT"], 36286)),
+    "user":      _e(["MYSQL_USER", "MYSQLUSER"], "root"),
+    "password":  _e(["MYSQL_PASSWORD", "MYSQLPASSWORD", "MYSQL_ROOT_PASSWORD"], "kZwsdDWOrUWwXDamELJNjAAoUnzCtROz"),
+    "db":        _e(["MYSQL_DATABASE"], "railway"),
     "cursorclass": pymysql.cursors.DictCursor
 }
