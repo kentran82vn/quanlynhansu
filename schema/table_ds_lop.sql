8a0dc9b650a0b825e87c1c2c3a53e62d057821fc
anhquannewbie <nicktran12345@gmail.com>
2025-07-15 02:32:57 +0700

    none

--- schema/table_ds_lop.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM quanlytruonghoc_app.lop;
-USE quanlytruonghoc_app;
--- Bảng danh sách lớp
-CREATE TABLE ds_lop (
-    ma_lop VARCHAR(255) PRIMARY KEY,
-    ten_lop VARCHAR(255)
-    );
