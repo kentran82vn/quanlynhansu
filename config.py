b36a15fcbcb2066c2887c5e6185046eb1388ddb9
anhquannewbie <nicktran12345@gmail.com>
2025-07-16 14:51:12 +0700

    a

--- config.py
+++ config.py
@@ -1,16 +1,8 @@
-import os, pymysql
-
-def _e(keys, default=None):
-    for k in keys:
-        v = os.environ.get(k)
-        if v: return v
-    return default
-
+import pymysql
 DB_CONFIG = {
-    "host":      _e(["MYSQL_HOST","MYSQLHOST"], "localhost"),
-    "port":      int(_e(["MYSQL_PORT","MYSQLPORT"], 3306)),
-    "user":      _e(["MYSQL_USER","MYSQLUSER"], "root"),
-    "password":  _e(["MYSQL_PASSWORD","MYSQLPASSWORD","MYSQL_ROOT_PASSWORD"], ""),
-    "db":        _e(["MYSQL_DATABASE"], "quanlytruonghoc_app"),
+    "host": "localhost",
+    "user": "root",
+    "password": "steven2906",  # đúng mật khẩu MySQL
+    "db": "quanlytruonghoc_app",  # ← sửa 'database' → 'db'
     "cursorclass": pymysql.cursors.DictCursor
 }
