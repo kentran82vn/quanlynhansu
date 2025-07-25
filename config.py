8937d7aa124c4872248d3c37247090205c108c43
anhquannewbie <nicktran12345@gmail.com>
2025-07-25 19:46:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os, pymysql
 
 def _e(keys, default=None):
@@ -6,11 +5,17 @@ def _e(keys, default=None):
         v = os.environ.get(k)
         if v: return v
     return default
+
 DB_CONFIG = {
-        "host":      _e(["MYSQL_HOST", "MYSQLHOST"], "shuttle.proxy.rlwy.net"),
-        "port":      int(_e(["MYSQL_PORT", "MYSQLPORT"], 36286)),
-        "user":      _e(["MYSQL_USER", "MYSQLUSER"], "root"),
-        "password":  _e(["MYSQL_PASSWORD", "MYSQLPASSWORD", "MYSQL_ROOT_PASSWORD"], "kZwsdDWOrUWwXDamELJNjAAoUnzCtROz"),
-        "db":        _e(["MYSQL_DATABASE"], "railway"),
-        "cursorclass": pymysql.cursors.DictCursor
-        }
+    # Dùng host local thay vì shuttle.proxy...
+    "host":      _e(["MYSQL_HOST", "MYSQLHOST"], "127.0.0.1"),
+    # Cổng MySQL mặc định
+    "port":      int(_e(["MYSQL_PORT", "MYSQLPORT"], 3306)),
+    # User local
+    "user":      _e(["MYSQL_USER", "MYSQLUSER"], "root"),
+    # Mật khẩu local
+    "password":  _e(["MYSQL_PASSWORD", "MYSQLPASSWORD", "MYSQL_ROOT_PASSWORD"], "173915Snow"),
+    # Tên database local
+    "db":        _e(["MYSQL_DATABASE"], "quanlytruonghoc_app"),
+    "cursorclass": pymysql.cursors.DictCursor
+}
